da2a1534cf135e946abc73d5ff7a33d52bdf4b21
elmagnifiko <abdoulrazakadamoubello@gmail.com>
2024-09-04 19:36:51 +0100

    update nextauth incompatibilite

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,10 +2,11 @@ import { env } from '@/lib/env';
 import { prisma } from '@/lib/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth, { AuthOptions } from 'next-auth';
+import { Adapter } from 'next-auth/adapters';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   theme: {
     logo: '/images/logo-text.png',
   },
