9d0631364af2d9d3697b178920f20211748456d6
elmagnifiko <abdoulrazakadamoubello@gmail.com>
2024-09-05 16:03:33 +0100

    install Tanstack Query - update globals.css - add components(cart toast and toaster) - add images logo of youcode and update package

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,12 +3,19 @@ import { SiteConfig } from '../../lib/site-config';
 import Link from 'next/link';
 import { ThemeToggle } from '../ThemeToggle';
 import { Typography } from '../ui/Typography';
+import Image from 'next/image';
 
 export function Header() {
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
+        <Image
+              src="/images/logo-text.png"
+              width={150}
+              height={35}
+              alt="app logo"
+            />
           <Typography variant="h3" as={Link} href="/">
             {SiteConfig.title}
           </Typography>
