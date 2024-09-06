ddf57ac37ea36adc507aab0b25c1f5bec448b51a
elmagnifiko <abdoulrazakadamoubello@gmail.com>
2024-09-06 02:37:25 +0100

    add others components from shadcn-ui , update prisma migration , update providers.tsx and update others packages

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,7 @@
 import { SiteConfig } from '../../lib/site-config';
 import Link from 'next/link';
 import { ThemeToggle } from '../ThemeToggle';
+import Image from 'next/image';
 import { Typography } from '../ui/Typography';
 
 export function Header() {
@@ -9,6 +10,7 @@ export function Header() {
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
+        <Image src="/images/logo.png" width={150} height={35} alt="app logo" />
           <Typography variant="h3" as={Link} href="/">
             {SiteConfig.title}
           </Typography>
